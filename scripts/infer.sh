35cc2f03476f9ee8cb6032f48757a92e44ddc561
karimibrahim11 <karim.ibrahim.amin@gmail.com>
2025-07-24 21:22:09 +0200

    Submodules Updates

--- scripts/infer.sh
+++ scripts/infer.sh
@@ -1,36 +1,37 @@
 
-# rm -rf output/SMPLH_meshes/tsdf-rgbd/*
-# rm -rf output/SMPLH_meshes/moved-meshes/*
-# rm -rf output/SMPL_meshes/*
-# rm -rf output/Close-output/*
-# mv output/3DGS_meshes/* output/backup_meshes/
+echo "Working on the image in inputs/images/"
+rm -rf output/SMPLH_meshes/tsdf-rgbd/*
+rm -rf output/SMPLH_meshes/moved-meshes/*
+rm -rf output/SMPL_meshes/*
+rm -rf output/Close-output/*
+mv output/3DGS_meshes/* output/backup_meshes/
 
-# echo "(*****************************************)"
-# echo "1- Inferring 3DGS using HUMAN3DDIFFUSION... "
-# cd Human3Diffusion
-# python infer.py --checkpoints checkpoints
-# python infer_mesh.py --checkpoints checkpoints --mesh_quality medium
-# cd ../
-# echo "Done!"
-# echo "(*****************************************)"
+echo "(*****************************************)"
+echo "1- Inferring 3DGS using HUMAN3DDIFFUSION... "
+cd Human3Diffusion
+python infer.py --checkpoints checkpoints
+python infer_mesh.py --checkpoints checkpoints --mesh_quality medium
+cd ../
+echo "Done!"
+echo "(*****************************************)"
 
-# echo "(*****************************************)"
-# echo "2- Fitting SMPLH using NICP... "
-# cd NICP
-# PYTHONPATH=. python ./src/lvd_templ/evaluation/evaluation_benchmark.py core.scaleback=True
-# cd ../
-# mv output/SMPLH_meshes/tsdf-rgbd/out_ss_cham_0.ply output/SMPLH_meshes/moved-meshes/out_ss.ply
-# echo "Done!"
-# echo "(*****************************************)"
+echo "(*****************************************)"
+echo "2- Fitting SMPLH using NICP... "
+cd NICP
+PYTHONPATH=. python ./src/lvd_templ/evaluation/evaluation_benchmark.py core.scaleback=True
+cd ../
+mv output/SMPLH_meshes/tsdf-rgbd/out_ss_cham_0.ply output/SMPLH_meshes/moved-meshes/out_ss.ply
+echo "Done!"
+echo "(*****************************************)"
 
-# echo "(*****************************************)"
-# echo "3- Fitting Converting SMPLH to SMPL... "
-# cd smplx
-# python -m transfer_model --exp-cfg config_files/smplh2smpl.yaml
-# cd ../
-# python scripts/plytoobj.py
-# echo "Done!"
-# echo "(*****************************************)"
+echo "(*****************************************)"
+echo "3- Fitting Converting SMPLH to SMPL... "
+cd smplx
+python -m transfer_model --exp-cfg config_files/smplh2smpl.yaml
+cd ../
+python scripts/plytoobj.py
+echo "Done!"
+echo "(*****************************************)"
 
 
 echo "(*****************************************)"
